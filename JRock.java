fe5754bbacb6d8b9de056d5d93266034d2e4db70
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2026-09-15 11:33:15 +0200

    JRock v1.3.1

--- JRock.java
+++ JRock.java
@@ -99,7 +99,7 @@ import java.util.List;
 public class JRock {
 
     // Application version.
-    private static final String VERSION = "1.3.0";
+    private static final String VERSION = "1.3.1";
 
     // ---- Configuration (mutable: changed via the Configure dialog) ----------
     private static final String DEFAULT_REGION = "us-east-1";
